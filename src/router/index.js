bcbd5459b108636dde233b167fadffd2133248aa
Rahman Islam <54945042+RahmanIslamIen@users.noreply.github.com>
2022-03-12 13:53:16 +0700

    pembuatan layout utama

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/latar-belakang",
+      name: "Latar Belakang Pendidikan",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      component: () => import("../views/LatarBelakang.vue"),
+    },
+    {
+      path: "/protofolio",
+      name: "protofolio",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/Protofolio.vue"),
+    },
+    {
+      path: "/sertifikasi",
+      name: "sertifikasi",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/Sertifikasi.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,50 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+import TheWelcome from "@/components/TheWelcome.vue";
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum, commodi,
+    dignissimos suscipit iusto pariatur excepturi amet sed praesentium
+    voluptatibus neque ipsam labore eveniet, ab eligendi dolor. Placeat error
+    praesentium distinctio nihil amet ratione alias ab sapiente non aut numquam
+    illo, nobis nulla! Quos odio reprehenderit accusamus nostrum perferendis
+    consectetur praesentium exercitationem corporis magnam cupiditate non
+    necessitatibus deleniti sit quibusdam, quo ea blanditiis nulla doloremque.
+    Blanditiis rerum, a nihil quia dolores laboriosam ut vitae consectetur,
+    architecto eos incidunt voluptas totam voluptatibus sequi in odio doloremque
+    aliquam commodi reiciendis esse cupiditate quasi! Dolorum consectetur dicta,
+    minima est quis quo nam minus voluptatibus quos distinctio cum qui voluptate
+    accusamus animi natus deserunt dignissimos a ipsam! Cupiditate sunt
+    obcaecati architecto unde, ex tempore voluptas ipsam expedita odit impedit
+    qui, esse doloremque sapiente, voluptatibus sequi et facilis. Ad nam
+    laboriosam suscipit reiciendis facere! Odit, officiis facere? Ullam porro
+    nihil quia atque laborum eos mollitia possimus totam iure ex soluta
+    similique laboriosam maxime recusandae in magni illo alias nemo, perferendis
+    impedit. Repudiandae iste, autem nobis impedit numquam repellat. Ducimus
+    dolorem maxime, repellat beatae dolorum velit sit optio at sunt impedit quos
+    nobis aut itaque, hic repudiandae ad fugiat facere quasi tenetur? Quidem
+    minima magni veniam autem commodi aperiam voluptatem quisquam eaque? Nulla
+    numquam, provident incidunt, adipisci impedit, asperiores dolore id omnis
+    sint minus architecto temporibus veritatis fugit repudiandae tempora
+    reiciendis recusandae nisi sed vitae delectus illo porro repellendus sunt
+    rerum! Hic, dolorum quam doloremque, dolores expedita ab iure, voluptate
+    laboriosam repellat iusto tempora. Sequi maxime eligendi recusandae, aliquid
+    aspernatur pariatur maiores ipsum reprehenderit esse incidunt tempore.
+    Maiores maxime facilis quia dignissimos unde quidem eaque blanditiis ipsa
+    iste distinctio, quibusdam, expedita laboriosam at in sit delectus minima
+    nostrum quod vitae. Quibusdam nam consequatur fugiat tenetur sed id dolorum
+    quas quod hic corporis doloribus enim quia placeat, obcaecati dolore eos
+    magnam mollitia molestias unde consectetur ab repellat. Provident ullam
+    repellendus ratione doloremque, perferendis dolorem ipsum vitae excepturi
+    optio eligendi aliquid laudantium, ipsa consequuntur odit assumenda quod?
+    Porro iusto quod temporibus eos sit, suscipit dolorem fugit! Aliquid
+    assumenda dolor molestias quas ut impedit, magni nemo consectetur nesciunt
+    amet asperiores esse, doloribus illum ducimus deserunt neque voluptas
+    accusamus nihil ipsam qui aspernatur. Quo id ut quia iste eum culpa
+    similique nisi fuga nostrum laborum. Necessitatibus est maiores quod commodi
+    doloribus reiciendis voluptatibus, vitae deserunt omnis, exercitationem,
+    eius quaerat minima! Repellat, reprehenderit unde. Illum laudantium quaerat
+    aut consequatur nesciunt. Officia, optio.
+  </p>
 </template>
