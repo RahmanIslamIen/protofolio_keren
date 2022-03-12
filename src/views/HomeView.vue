eb3d60ea86b5dc857cb3c5a6d85251a5097fb587
Rahman Islam <54945042+RahmanIslamIen@users.noreply.github.com>
2022-03-12 14:51:45 +0700

    penambahan paket

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,48 +3,25 @@ import TheWelcome from "@/components/TheWelcome.vue";
 </script>
 
 <template>
+  <h3>Perkenalan</h3>
   <p>
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum, commodi,
-    dignissimos suscipit iusto pariatur excepturi amet sed praesentium
-    voluptatibus neque ipsam labore eveniet, ab eligendi dolor. Placeat error
-    praesentium distinctio nihil amet ratione alias ab sapiente non aut numquam
-    illo, nobis nulla! Quos odio reprehenderit accusamus nostrum perferendis
-    consectetur praesentium exercitationem corporis magnam cupiditate non
-    necessitatibus deleniti sit quibusdam, quo ea blanditiis nulla doloremque.
-    Blanditiis rerum, a nihil quia dolores laboriosam ut vitae consectetur,
-    architecto eos incidunt voluptas totam voluptatibus sequi in odio doloremque
-    aliquam commodi reiciendis esse cupiditate quasi! Dolorum consectetur dicta,
-    minima est quis quo nam minus voluptatibus quos distinctio cum qui voluptate
-    accusamus animi natus deserunt dignissimos a ipsam! Cupiditate sunt
-    obcaecati architecto unde, ex tempore voluptas ipsam expedita odit impedit
-    qui, esse doloremque sapiente, voluptatibus sequi et facilis. Ad nam
-    laboriosam suscipit reiciendis facere! Odit, officiis facere? Ullam porro
-    nihil quia atque laborum eos mollitia possimus totam iure ex soluta
-    similique laboriosam maxime recusandae in magni illo alias nemo, perferendis
-    impedit. Repudiandae iste, autem nobis impedit numquam repellat. Ducimus
-    dolorem maxime, repellat beatae dolorum velit sit optio at sunt impedit quos
-    nobis aut itaque, hic repudiandae ad fugiat facere quasi tenetur? Quidem
-    minima magni veniam autem commodi aperiam voluptatem quisquam eaque? Nulla
-    numquam, provident incidunt, adipisci impedit, asperiores dolore id omnis
-    sint minus architecto temporibus veritatis fugit repudiandae tempora
-    reiciendis recusandae nisi sed vitae delectus illo porro repellendus sunt
-    rerum! Hic, dolorum quam doloremque, dolores expedita ab iure, voluptate
-    laboriosam repellat iusto tempora. Sequi maxime eligendi recusandae, aliquid
-    aspernatur pariatur maiores ipsum reprehenderit esse incidunt tempore.
-    Maiores maxime facilis quia dignissimos unde quidem eaque blanditiis ipsa
-    iste distinctio, quibusdam, expedita laboriosam at in sit delectus minima
-    nostrum quod vitae. Quibusdam nam consequatur fugiat tenetur sed id dolorum
-    quas quod hic corporis doloribus enim quia placeat, obcaecati dolore eos
-    magnam mollitia molestias unde consectetur ab repellat. Provident ullam
-    repellendus ratione doloremque, perferendis dolorem ipsum vitae excepturi
-    optio eligendi aliquid laudantium, ipsa consequuntur odit assumenda quod?
-    Porro iusto quod temporibus eos sit, suscipit dolorem fugit! Aliquid
-    assumenda dolor molestias quas ut impedit, magni nemo consectetur nesciunt
-    amet asperiores esse, doloribus illum ducimus deserunt neque voluptas
-    accusamus nihil ipsam qui aspernatur. Quo id ut quia iste eum culpa
-    similique nisi fuga nostrum laborum. Necessitatibus est maiores quod commodi
-    doloribus reiciendis voluptatibus, vitae deserunt omnis, exercitationem,
-    eius quaerat minima! Repellat, reprehenderit unde. Illum laudantium quaerat
-    aut consequatur nesciunt. Officia, optio.
+    Saya seorang perekayasa perangkat lunak dengan lebih dari dua dekade
+    pengalaman dalam bidang industri teknik informatika, menyediakan keahlian
+    berharga bagi bisnis rintisan.
   </p>
+  <h3>Bidang Keahlian</h3>
+  <ul>
+    <li>
+      Saya pernah mengerjakan beragam jenis proyek serta telah menguasai
+      beberapa bahasa pemrograman dan pengodean sekaligus pengujian dan
+      perbaikan perangkat lunak.
+    </li>
+    <li>berpengalaman di bidang hardware</li>
+    <li>WordPress theme developer membuat tema pada website wordpress</li>
+    <li>
+      Content Writer penulisan berbasis SEO serta memberikan konten yang sesuai
+      dengan keinginan dan dapat membantu para pembaca
+    </li>
+  </ul>
+  <h3>Riwayat pekerjaan</h3>
 </template>
