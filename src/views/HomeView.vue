49db2ab47d0366f3b946c9146fbc7f4b148915ca
Rahman Islam <54945042+RahmanIslamIen@users.noreply.github.com>
2022-03-23 15:50:22 +0700

    pembaharuan layout utama

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,55 +3,87 @@ import TheWelcome from "@/components/TheWelcome.vue";
 </script>
 
 <template>
-  <h3>Perkenalan</h3>
-  <p>
-    Saya seorang perekayasa perangkat lunak dengan lebih dari dua dekade
-    pengalaman dalam bidang industri teknik informatika, menyediakan keahlian
-    berharga bagi bisnis rintisan.
-  </p>
-  <h3>Bidang Keahlian</h3>
-  <ul>
-    <li>
-      Saya pernah mengerjakan beragam jenis proyek serta telah menguasai
-      beberapa bahasa pemrograman dan pengodean sekaligus pengujian dan
-      perbaikan perangkat lunak.
-    </li>
-    <li>berpengalaman di bidang hardware</li>
-    <li>WordPress theme developer membuat tema pada website wordpress</li>
-    <li>
-      Content Writer penulisan berbasis SEO serta memberikan konten yang sesuai
-      dengan keinginan dan dapat membantu para pembaca
-    </li>
-  </ul>
+  <table>
+    <tr>
+      <td>
+        <img
+          alt="Gambar Profile"
+          src="../assets/RahmanIslam_profile.jpg"
+          class="gambarProfile"
+        />
+      </td>
+      <td class="keterangan">
+        <h3>Perkenalan</h3>
+        <p>
+          Saya seorang perekayasa perangkat lunak dengan lebih dari dua dekade
+          pengalaman dalam bidang industri teknik informatika, menyediakan
+          keahlian berharga bagi bisnis rintisan.
+        </p>
 
-  <h3>KONTAK</h3>
-  <h4>Alamat:</h4>
-  <p>Jakarta Selatan</p>
-  <h4>Telepon / WA :</h4>
-  <p>0101010101</p>
+        <h3>Bidang Keahlian</h3>
+        <ul>
+          <li>
+            Saya pernah mengerjakan beragam jenis proyek serta telah menguasai
+            beberapa bahasa pemrograman dan pengodean sekaligus pengujian dan
+            perbaikan perangkat lunak.
+          </li>
+          <li>berpengalaman di bidang hardware</li>
+          <li>WordPress theme developer membuat tema pada website wordpress</li>
+          <li>
+            Content Writer penulisan berbasis SEO serta memberikan konten yang
+            sesuai dengan keinginan dan dapat membantu para pembaca
+          </li>
+        </ul>
+      </td>
+    </tr>
+  </table>
 
-  <h3>KEAHLIAN</h3>
-  <ul>
-    <li>HARDWARE</li>
-    <li>INSTALASI SOFTWARE</li>
-    <li>TESTING JARINGAN</li>
-    <li>PHP, JAVA, Java Script, C++, CSS3, SQL</li>
-  </ul>
+  <table class="infoLengkap">
+    <tr>
+      <td rowspan="2">
+        <div class="infoSelengkapnya">
+          <h3>KONTAK</h3>
+          <h4>Alamat:</h4>
+          <p>Jakarta Selatan</p>
+          <h4>Telepon / WA :</h4>
+          <p>0101010101</p>
+        </div>
+      </td>
+      <td>
+        <div class="infoSelengkapnya">
+          <h3>KEAHLIAN</h3>
+          <ul>
+            <li>HARDWARE</li>
+            <li>INSTALASI SOFTWARE</li>
+            <li>TESTING JARINGAN</li>
+            <li>PHP, JAVA, Java Script, C++, CSS3, SQL</li>
+          </ul>
+        </div>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <div class="infoSelengkapnya">
+          <h3>HOBI</h3>
+          <ul>
+            <li>NGODING</li>
+            <li>BLOGING</li>
+            <li>MEMBACA</li>
+          </ul>
+        </div>
+      </td>
+    </tr>
+  </table>
 
-  <h3>HOBI</h3>
-  <ul>
-    <li>NGODING</li>
-    <li>BLOGING</li>
-    <li>MEMBACA</li>
-  </ul>
-
-  <h3>Riwayat pekerjaan</h3>
-  <h4>LAB ICT UBL || UNIVERSITAS BUDILUHUR</h4>
-  <p>
-    saya mempunyai pengalaman praktek kerja lapangan di universitas budi luhur
-    pekerjaan saya disana adalah memperbaiki sofware komputer dan berbagai
-    masalah lain nya yang ada pada suatu komputer serta mengerjakan karya tulis
-    yang akan disidang setelah meyelesaikan kegiatan PKL yang berlangsung selama
-    2 bulan.
-  </p>
+  <div class="kontenRiwayat">
+    <h3><u>Riwayat pekerjaan</u></h3>
+    <h4>LAB ICT UBL || UNIVERSITAS BUDILUHUR</h4>
+    <p>
+      saya mempunyai pengalaman praktek kerja lapangan di universitas budi luhur
+      pekerjaan saya disana adalah memperbaiki sofware komputer dan berbagai
+      masalah lain nya yang ada pada suatu komputer serta mengerjakan karya
+      tulis yang akan disidang setelah meyelesaikan kegiatan PKL yang
+      berlangsung selama 2 bulan.
+    </p>
+  </div>
 </template>
